5f186aa46b8d2d99995ae09deb3efbe0894274e8
Noble Obioma <nobioma1@gmail.com>
2020-02-21 22:17:39 +0100

    setup react native project

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+class App extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Hello World</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({});
+
+export default App;

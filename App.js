59431b1f85089c7c4bc43008b0a80d3eb4d4b500
Noble Obioma <nobioma1@gmail.com>
2020-02-23 21:42:10 +0100

    Setup screen navigation

--- App.js
+++ App.js
@@ -1,16 +1,39 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, SafeAreaView, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import FormView from './components/FormView';
+import EditProfile from './components/EditProfile';
+import Flex from './components/Flex';
+
+const Stack = createStackNavigator();
 
 class App extends React.Component {
   render() {
     return (
-      <View>
-        <Text>Hello World</Text>
-      </View>
+      <NavigationContainer>
+        <SafeAreaView style={styles.appContainer}>
+          <Stack.Navigator
+            screenOptions={{
+              headerShown: false,
+            }}>
+            <Stack.Screen name="Form" component={FormView} />
+            <Stack.Screen name="Profile" component={EditProfile} />
+          </Stack.Navigator>
+        </SafeAreaView>
+      </NavigationContainer>
     );
   }
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  appContainer: {
+    marginTop: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#FFF',
+    flex: 1,
+  },
+});
 
 export default App;
